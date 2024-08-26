2e3f22b7c930cfeb0368860a50af3c2784f69788
murillodominguez <101913723+murillodominguez@users.noreply.github.com>
2024-08-26 16:54:20 -0300

    new commit

--- eventif/settings.py
+++ eventif/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-38#=(j=v0vrup92aky&8f9&7+(3t3khfdt)6p-$_hl-1d#*a0i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
