a00a728c424c1c6dca20605a82821e5b0626acbd
murillodominguez <101913723+murillodominguez@users.noreply.github.com>
2024-10-07 16:42:49 -0300

    habilita inscricao

--- core/tests.py
+++ core/tests.py
@@ -10,3 +10,6 @@ class HomeTest(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_link_subscription(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
